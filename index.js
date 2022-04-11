8995fa788e10dc2d5ffdb12c20114d3fb1463bb3
desmond <desmond@desmonds-MacBook-Pro.local>
2022-04-11 15:42:07 +0300

    task complete

--- index.js
+++ index.js
@@ -1,1 +1,66 @@
 // Write your solution in this file!
+const employee = {name:"desmond", streetAddress: "komboni road"};
+
+beforeEach(function () {
+  for (const key in employee) {
+    delete employee[key];
+  }
+
+  
+});
+
+
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  let coemployee= { ...employee};
+  coemployee[key] = value;
+  return coemployee;
+}
+
+beforeEach(function () {
+  for (const key in employee) {
+    delete employee[key];
+  }
+
+  
+});
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+
+beforeEach(function () {
+  for (const key in employee) {
+    delete employee[key];
+  }
+
+  
+});
+
+
+function deleteFromEmployeeByKey(employee, key) {
+
+  let coemployee= { ...employee};
+  delete coemployee[key];
+  return coemployee;
+}
+
+
+beforeEach(function () {
+  for (const key in employee) {
+    delete employee[key];
+  }
+
+  
+});
+
+
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+  
+  delete employee[key];
+  
+  return employee;
+}
